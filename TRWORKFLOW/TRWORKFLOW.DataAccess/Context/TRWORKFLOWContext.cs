6039c5b065ffc329d1aa8f35d6a8f82816212756
Halim Bilici <halimbilici2022@gmail.com>
2023-04-25 16:00:36 +0300

    Database bağlantısı için ilgili classlar oluşturuldu.

---
+++ TRWORKFLOW/TRWORKFLOW.DataAccess/Context/TRWORKFLOWContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRWORKFLOW.Entities.Concrete;
+
+namespace TRWORKFLOW.DataAccess.Context
+{
+    public class TRWORKFLOWContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=HLMBLC-NW\\SQLEXPRESS;Database=TRWORKFLOW;Trusted_Connection=true;");
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserDetail> UserDetails { get; set; }
+    }
+}
